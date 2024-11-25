0b973e4a156bd8da33cef3f8d3409d20cb740f1a
nataliaElv <natalia.elvira@huggingface.co>
2024-11-25 13:43:42 +0100

    Quartz sync: Nov 25, 2024, 1:43 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -36,6 +36,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
+    Component.TagList(),
   ],
 }
 
