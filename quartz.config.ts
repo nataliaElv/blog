24e57da9c549bebc9cbf0705b29f1fbc3da5149d
nataliaElv <natalia.elvira@huggingface.co>
2024-11-25 13:33:35 +0100

    Quartz sync: Nov 25, 2024, 1:33 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -19,6 +19,7 @@ const config: QuartzConfig = {
     baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
+    generateSocialImages: true,
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
